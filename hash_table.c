8ff443316f059bca6d2e63dcac8aeab7f073b6f0
Ethan <ethanrodkin@gmail.com>
2018-04-23 17:17:30 -0400

    Functionality mostly there. Resize is losing values for some reason.

--- hash_table.c
+++ hash_table.c
@@ -1,24 +1,14 @@
 #include <string.h>
 #include <stdio.h>
 #include <stdbool.h>
+#include <stdlib.h>
 #include "hash_table.h"
 #define MODULO 337
 
-// int main() {
-//     return 0;
-// }
+int hash_size = 16;
 
-/* Things to add:
-1) find_key_in_hash_table() function. We're searching for a key in the hash table
-in the insert, delete, and search functions. All three of those functions could
-just call a single function which returns a pointer to the K_V_Pair if the key
-does exist, and NULL otherwise. -- DONE --
-2) Freeing! We gotta free more than just a K_V_Pair. We should also free
-the key and value themselves. Make a free_all() function, call that instead of
-free.
-*/
 
-Hash_Table* hash_table() {
+Hash_Table* new_HT() {
     Hash_Table *new;
     new = (Hash_Table*) malloc(sizeof(Hash_Table));
 
@@ -31,35 +21,37 @@ Hash_Table* hash_table() {
 
 
 int hash_index(char* key, int size) {
-    int hash = 0;
     int k_index = 1;
     int index = 0;
 
     while (*key) {
         index += (int) *(key)++ * k_index++;
     }
-    int index = (index % MODULO) % size;
+    index = (index % MODULO) % size;
+    // printf("Index is %d\n", index);
     return index;
 }
 
-void resize(Hash_Table* dict) {
+Hash_Table* resize(Hash_Table* dict) {
     // resizes dict, either doubling it if current items >= 75% of dict size,
     // or halfing it if current items <= 25% of dict size. Reassigns indices
     // based on current size.
+    printf("Resize starts\n");
     if (dict->cur_size > 3 * dict->cur_items) {
         hash_size = dict->cur_size / 2; // change hash size for resizing purposes.
     } else if (dict->cur_items * 4 >= dict->cur_size * 3){
         hash_size = dict->cur_size * 2;
     } else {
         printf("Error. Resize should not have been called.");
-        return;
+        return dict;
     }
-    Hash_Table* new_dict = hash_table();
+    Hash_Table* new_dict = new_HT();
     for (int i = 0; i < dict->cur_size; ++i) {
-        K_V_Pair *current = dict[i];
+        K_V_Pair *current = dict->entries[i];
         while (current) {
+            new_dict->cur_items++;
             K_V_Pair *next = current->next;
-            int new_index = hash_index(current->key, new_dict->size);
+            int new_index = hash_index(current->key, hash_size);
             current->next = NULL;
             current->prev = NULL;
             assign_index(new_dict, current, new_index);
@@ -68,7 +60,8 @@ void resize(Hash_Table* dict) {
     }
     free(dict);
     hash_size = 16; // reset hash size to default for a new hash.
-    return new;
+    printf("We're done!\n");
+    return new_dict;
 }
 
 K_V_Pair* new_dict_entry(char *key, char *value) {
@@ -82,10 +75,11 @@ K_V_Pair* new_dict_entry(char *key, char *value) {
     return new;
 }
 
-void insert(Hash_Table* dict, char* key, char* value) {
+void hash_insert(Hash_Table* dict, char* key, char* value) {
     // Checks if we need to resize the dict, and calls resize function if so.
     // Then, it generates a new key-value pair and hashes the key to find an
     // index. Then, calls the assign_index function.
+    // dict = resize(dict);
     if (++dict->cur_items > ((dict->cur_size * 75) / 100)) {
         dict = resize(dict);
     }
@@ -93,46 +87,36 @@ void insert(Hash_Table* dict, char* key, char* value) {
     K_V_Pair *new_pair;
     new_pair = find_key_in_hash_table(dict, key, i);
     if (new_pair) {
-        free(new_pair->value);
+        // free(new_pair->value);
         new_pair->value = value;
         return;
     }
     new_pair = new_dict_entry(key, value);
-    if (!dict[i]) {
-        dict[i] = new_pair;
-        return;
-    }
-    K_V_Pair* current = dict[i];
-    while (current) {
-        if (!current->next) {
-            current->next = new_pair;
-            new_pair->prev = current;
-            return;
-        }
-        current = current->next;
-    }
-    printf("Error. You should never reach here!\n");
+    assign_index(dict, new_pair, i);
 }
 
-void remove(Hash_Table* dict, char* key) {
+void hash_remove(Hash_Table* dict, char* key) {
     // searches for the key in the hash table, and deletes it.
+    // dict = resize(dict);
     if (--dict->cur_items * 4 <= dict->cur_size) {
         dict = resize(dict);
     }
-    int index = hash_index(key, dict->size);
+    int index = hash_index(key, dict->cur_size);
     K_V_Pair* to_remove = find_key_in_hash_table(dict, key, index);
     if (to_remove) {
-        if (!to_remove->prev) {
-            dict[index] = to_remove->next;
-            to_remove->next->prev = NULL;
-        } else {
+        if (to_remove->prev) {
             to_remove->prev->next = to_remove->next;
             if (to_remove->next) {
                 to_remove->next->prev = to_remove->prev;
             }
+        } else {
+            dict->entries[index] = to_remove->next;
+            if (to_remove->next) {
+                to_remove->next->prev = NULL;
+            }
         }
-        free(to_remove->key);
-        free(to_remove->value);
+        // free(to_remove->key);
+        // free(to_remove->value);
         free(to_remove);
         return;
     }
@@ -145,11 +129,13 @@ void assign_index(Hash_Table* dict, K_V_Pair* new_pair, int i) {
     // Assigns a pair to an index. If key is already in the table, then the
     // key's associated value is changed and that's it. If the index is already
     // occupied, then we link that list!
-    if (dict[i]) { // index is already occupied
-        K_V_Pair *current = dict[i];
+    // dict->cur_items++;
+    if (dict->entries[i]) { // index is already occupied
+        K_V_Pair *current = dict->entries[i];
         while (current) {
             if (strcmp(current->key, new_pair->key) == 0) {
                 current->key = new_pair->key;
+                // free(new_pair->value);
                 free(new_pair);
                 return;
             }
@@ -161,24 +147,25 @@ void assign_index(Hash_Table* dict, K_V_Pair* new_pair, int i) {
             current = current->next;
         }
     } else {
-        dict->values[i] = new_pair;
+        dict->entries[i] = new_pair;
     }
 }
 
 char* hash_search(Hash_Table* dict, char* key) {
-    int index = hash_index(key, dict->size);
+    int index = hash_index(key, dict->cur_size);
     K_V_Pair* hash_pair = find_key_in_hash_table(dict, key, index);
     if (hash_pair) {
+        printf("%s\n", hash_pair->value);
         return hash_pair->value;
     }
-    printf("Key not found in dictionary.");
+    printf("Search failed; key not found in dictionary.\n");
     return NULL;
 }
 
 
 K_V_Pair* find_key_in_hash_table(Hash_Table* dict, char* key, int index) {
-    if (dict[index]) {
-        K_V_Pair* current = dict[index];
+    if (dict->entries[index]) {
+        K_V_Pair* current = dict->entries[index];
         while (current) {
             if (strcmp(current->key, key) == 0) {
                 return current;
@@ -187,4 +174,10 @@ K_V_Pair* find_key_in_hash_table(Hash_Table* dict, char* key, int index) {
         }
     }
     return NULL;
+}
+
+int table_size(Hash_Table* dict) {
+    printf("Current items: %d\n", dict->cur_items);
+    printf("Current size: %d\n", dict->cur_size);
+    return dict->cur_items;
 }
--- hash_table.h
+++ hash_table.h
@@ -1,36 +1,35 @@
 #ifndef hash_table
 #define hash_table
 
-Hash_Table* hash_table(int size);
-Hash_Table* resize(Hash_Table* dict);
-void remove(Hash_Table* dict, char* key);
-void hash_insert(char* key, char* value);
-int hash_index(char* key), int size;
-void assign_index(Hash_Table* dict, K_V_Pair* pair);
-char* hash_search(Hash_Table* dict, char* key);
-K_V_Pair* find_key_in_hash_table(Hash_Table* dict, char* key);
-
-int hash_size = 16;
-
-
-typedef union {
-    int value_int;
-    char *value_char;
-    bool val_bool;
-    char *type;
-} Key;
-
-typedef struct {
+typedef struct K_V_Pair {
     char *key;
     char *value;
-    K_V_Pair* prev;
-    K_V_Pair* next;
+    struct K_V_Pair *prev;
+    struct K_V_Pair *next;
 } K_V_Pair;
 
-typedef struct {
+typedef struct Hash_Table {
     int cur_size;
     int cur_items;
     K_V_Pair** entries;
 } Hash_Table;
+
+Hash_Table* new_HT();
+Hash_Table* resize(Hash_Table *dict);
+void hash_remove(Hash_Table *dict, char *key);
+void hash_insert(Hash_Table* dict, char *key, char *value);
+int hash_index(char *key, int size);
+void assign_index(Hash_Table *dict, K_V_Pair *pair, int index);
+char* hash_search(Hash_Table *dict, char *key);
+K_V_Pair* find_key_in_hash_table(Hash_Table *dict, char *key, int index);
+int table_size(Hash_Table* dict);
+
+
+// typedef union {
+//     int value_int;
+//     char *value_char;
+//     bool val_bool;
+//     char *type;
+// } Key;
 
 #endif
